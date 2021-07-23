b596c8fd57aa4a37013856cd5e298c7d2f51ecc3
Sa Htet Wai Aung <87661002+Sahtetwaiaung@users.noreply.github.com>
2021-07-23 11:35:56 +0630

    Update email.php

--- email.php
+++ email.php
@@ -1,18 +1,3 @@
 <?php
 echo "sending email";
 echo "another email sent";
-
-echo "third line from git";
-
-echo "third line from online";
-
-echo "fourth line from cli";
-
-echo "fourth line from online":
-
-echo "fifth line from online";
-
-
-
-echo "fifth line from cli";
-
